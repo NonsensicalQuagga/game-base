2114c40fe6759ad12a5d4e71bfad603d4e4b5854
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-10-10 11:11:55 +0200

    less clipping into the border of the screen

--- src/Player.js
+++ src/Player.js
@@ -13,15 +13,15 @@ export default class player{
 
     update(deltaTime){
         if (this.game.keys.includes('ArrowLeft') &&
-        !(this.x < 0)) this.speedX = -this.maxSpeed;
+        !(this.x <= 0)) this.speedX = -this.maxSpeed;
         else if (this.game.keys.includes('ArrowRight') &&
-        !(this.x + this.width > this.game.width)) this.speedX = this.maxSpeed;
+        !(this.x + this.width >= this.game.width)) this.speedX = this.maxSpeed;
         else this.speedX = 0;
 
         if (this.game.keys.includes('ArrowUp') &&
-        !(this.y < 0) ) this.speedY = -this.maxSpeed;
+        !(this.y <= 0) ) this.speedY = -this.maxSpeed;
         else if (this.game.keys.includes('ArrowDown') &&
-        !(this.y + this.height > this.game.height)) this.speedY = this.maxSpeed;
+        !(this.y + this.height >= this.game.height)) this.speedY = this.maxSpeed;
         else this.speedY = 0;
         
         
