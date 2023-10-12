b5da122c2b2f55f8c6374a61cda2462e87255df2
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-10-12 10:55:30 +0200

    game now has some ui

--- src/Game.js
+++ src/Game.js
@@ -1,15 +1,18 @@
 import InputHandler from "./InputHandler";
 import player from "./Player";
+import UserInterface from "./UserInterface";
 export default class Game {
   constructor(width, height) {
     this.width = width;
     this.height = height;
-    this.input = new InputHandler(this)
+    this.input = new InputHandler(this);
+    this.ui = new UserInterface(this);
     this.keys = [];
     this.enemies = [];
     this.gameOver = false;
     this.gravity = 1;
     this.debug = false;
+    this.gameTime = 0;
 
     this.player = new player(this);
   }
@@ -23,5 +26,6 @@ export default class Game {
 
   draw(context) {
     this.player.draw(context);
+    this.ui.draw(context);
   }
 }
