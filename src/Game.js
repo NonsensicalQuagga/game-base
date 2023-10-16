113f8f83209b336db1b32c31cef844bdfbc25428
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-10-16 12:59:59 +0200

    created enemies (they can't die yet)

--- src/Game.js
+++ src/Game.js
@@ -1,6 +1,7 @@
 import InputHandler from "./InputHandler";
 import player from "./Player";
 import UserInterface from "./UserInterface";
+import Slime from "./Slime";
 export default class Game {
   constructor(width, height) {
     this.width = width;
@@ -8,11 +9,14 @@ export default class Game {
     this.input = new InputHandler(this);
     this.ui = new UserInterface(this);
     this.keys = [];
-    this.enemies = [];
     this.gameOver = false;
     this.gravity = 1;
     this.debug = false;
     this.gameTime = 0;
+
+    this.enemies = [];
+    this.enemyTimer = 0;
+    this.enemyInterval = 1000;
 
     this.player = new player(this);
   }
@@ -22,10 +26,26 @@ export default class Game {
       this.gameTime += deltaTime;
     }
     this.player.update(deltaTime);
+
+    if (this.enemyTimer > this.enemyInterval && !this.gameOver) {
+      this.addEnemy();
+      this.enemyTimer = 0;
+    } else {
+      this.enemyTimer += deltaTime;
+    }
+
+    this.enemies.forEach((enemy) => enemy.update(deltaTime));
+    this.enemies = this.enemies.filter((enemy) => !enemy.markedForDeletion);
   }
 
   draw(context) {
     this.player.draw(context);
     this.ui.draw(context);
+    this.enemies.forEach((enemy) => enemy.draw(context));
   }
+
+  addEnemy() {
+    this.enemies.push(new Slime(this))
+  }
+
 }
