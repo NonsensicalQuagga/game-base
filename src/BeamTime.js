1ae0aea8c50aa67d0d6298a4290e1ce3b566721c
NonsensicalQuagga <alvin.hogdal@elev.ga.ntig.se>
2023-11-30 11:12:42 +0100

    Powerups have sprites

--- src/BeamTime.js
+++ src/BeamTime.js
@@ -1,4 +1,5 @@
 import Enemy from "./Enemy";
+import spriteImage from "./assets/sprites/pumpkinJosef.png";
 
 export default class BeamTime extends Enemy {
     constructor(game) {
@@ -6,13 +7,34 @@ export default class BeamTime extends Enemy {
         this.collisionDamage = 0;
         this.x = game.width;
         this.y = 60;
-        this.width = 20;
-        this.height = 20;
+        this.width = 50;
+        this.height = 50;
         this.color = '#08c9c3';
         this.scorePoints = 20000;
         this.speedX = -3;
         this.damageincrease = 0.5;
         this.lives = -69;
+
+        this.hasSprite = true;
+
+        const image = new Image();
+        image.src = spriteImage;
+        this.image = image;
+
+        this.frameX = 0;
+        this.frameY = 0;
+        this.maxFrame = 9;
+        this.fps = 9;
+        this.timer = 0;
+        this.interval = 1000 / this.fps;
+        this.flip = true;
+        this.walkingFrames = 1;
+        this.walkingPosition = 0;
+        this.idleFrames = 9;
+        this.idlePosition = 0;
+        this.scale = 1;
+        this.ofsetX = 5;
+        this.ofsetY = 10;
     }
 
     effect() {
